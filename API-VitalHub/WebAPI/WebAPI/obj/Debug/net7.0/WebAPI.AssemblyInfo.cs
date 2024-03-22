1cd1fdf305c0729666ec3ec2b31217d46fc3e378
Eduardo Felipe <48114302801@info.local>
2024-03-22 15:22:08 -0300

    Mudado IP api

--- API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80acf84ffb5f6a7486444f20f763d9ccdf4a6259")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989d9741c15b559a526acfb5dc63267589986882")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
